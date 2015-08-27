915578149a12fa2991c640258cf1db92a85d23ea
James Earle <j_earle@hotmail.com>
2015-08-27 01:52:23 -0400

    reverse in C++, permutation in java

--- 1/OneTwo.cpp
+++ 1/OneTwo.cpp
@@ -7,22 +7,40 @@ terminated string.
 James Earle - August 26, 2015
 */
 
-#include <string> 
+#include <string.h> 
 #include <iostream> 
 using namespace std; 
 void reverse(char*); 
 
 int main() 
 {
+	cout << "Enter a string: ";
+
 	//100 Characters seems like enough.
  	char str[101];
- 	cin >> str;
+
+ 	//Gets user input into char array, while getline(cin, str_name) gets for string type
+ 	cin.getline(str,101);
+
+ 	int size = strlen(str);
+ 	cout << size << endl;
+
  	reverse(str); 
+ 	cout << str << endl;
 
  	return 0; 
 }
 
 void reverse(char* str) 
-{ 
-	cout << str;
+{ 	
+	char* start = str;
+	char* end = start + strlen(str) - 1;
+	
+	while(end > start) {
+		char temp = *end;
+		*end = *start;
+		*start = temp;
+		start++;
+		end--;
+	}
 } 
